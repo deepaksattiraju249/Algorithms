6b5b4fdb51028b890ee2a78d7067dbc390b01889
Deepak Sattiraju <deepaks.iiti@gmail.com>
2014-05-14 23:02:00 +0530

    Stacks and Queues

---
+++ Stacks and Queues/Stack&Queue.py
@@ -0,0 +1,13 @@
+class Stack:
+    stack = []
+    def push(val):
+        stack.append(val)
+    def pop(val):
+        stack.pop(-1)
+
+class Queue:
+    queue = []
+    def enqueue(val):
+        queue.append(val)
+    def dequeue:
+        queue.pop(0)
