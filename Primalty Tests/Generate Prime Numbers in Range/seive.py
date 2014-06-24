5fe6c7dd15ea560d4899b7b127fe74232d5800ef
Deepak Sattiraju <deepaks.iiti@gmail.com>
2014-06-24 19:14:46 +0530

    ----

--- Primalty Tests/Generate Prime Numbers in Range/seive.py
+++ Primalty Tests/Generate Prime Numbers in Range/seive.py
@@ -1,21 +1,28 @@
-n = input()
-ListOfItems = []
-for i in xrange(n+1):
-	ListOfItems.append(True)
-print ListOfItems
-maxIvalue = int(n**0.5)
-print maxIvalue
-for i in xrange(2,maxIvalue +1):
-	print i
-	if ListOfItems[i] == True:
-		print "reached here"
-		j = i*i
-		while j < n:
-			#print j 
-			ListOfItems[j] = False
-			j += i
-print ListOfItems
 
-for i in xrange(2,n):
-	if(ListOfItems[i]):
-		print i
+def printPrimes(ListOfItems, m,n):
+	if (n < len(ListOfItems)):
+		for i in xrange(m,n):
+			if(ListOfItems[i]):
+				print i
+		return ListOfItems
+	ListOfItems = []
+	for i in xrange(n+1):
+		ListOfItems.append(True)
+	maxIvalue = int(n**0.5)
+	for i in xrange(2,maxIvalue +1):
+		if ListOfItems[i] == True:
+			j = i*i
+			while j < n:
+				ListOfItems[j] = False
+				j += i
+	for i in xrange(m,n):
+		if(ListOfItems[i]):
+			print i
+	return ListOfItems
+
+ListOfItems = []
+t = input()
+for i in range(t):
+	s = raw_input().split(" ")
+	ListOfItems = printPrimes(ListOfItems, int(s[0]),int(s[1]))
+	print ""
