9a7af1a13b2d6df8befa9e904bdc7791efa0292d
deepak.sattiraju <deepak.sattiraju@snapdeal.com>
2016-02-03 15:13:59 +0530

    Euler 001.py

---
+++ Euler_hackerrank/001.py
@@ -0,0 +1,41 @@
+'''
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below N.
+
+Input Format 
+First line contains T that denotes the number of test cases. This is followed by T lines, each containing an integer, N.
+
+Output Format 
+For each test case, print an integer that denotes the sum of all the multiples of 3 or 5 below N.
+
+Constraints 
+1≤T≤105 
+1≤N≤109
+
+Sample Input
+
+2
+10
+100
+
+Sample Output
+
+23
+2318
+'''
+
+
+
+
+def s(n):
+    return n*(n+1) /2
+
+t = input()
+for _ in xrange(t):
+    n = input() -1
+    n3 = n/3
+    n5 = n/5
+    n15 = n/15
+    
+    print 3*s(n3) + 5*s(n5) - 15*s(n15)
